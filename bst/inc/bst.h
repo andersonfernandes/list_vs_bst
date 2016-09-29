f97f6e7efe79a02c04c626ca9dc770d1d7d29680
Anderson Fernandes <fernandesanderson14@gmail.com>
2016-09-29 15:08:44 -0300

    Refatorada implemeentação de bst
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- bst/inc/bst.h
+++ bst/inc/bst.h
@@ -3,14 +3,16 @@
 
   typedef struct binarytree BinaryTree;
 
+  BinaryTree* create_empty_bst();
+
   BinaryTree* create_bst_node(int item);
 
-  BinaryTree* add_branches(BinaryTree* bt, int item);
+  BinaryTree* add_branch(BinaryTree* bt, int item);
 
   void free_bst(BinaryTree* bt);
-  
-  int search_bst(BinaryTree* bt, int item);
-  
+
+  BinaryTree* search_bst(BinaryTree* bt, int item);
+
   BinaryTree* remove_bst_node(BinaryTree* bt, int item);
 
   void print_bst(BinaryTree* bt);
--- bst/src/bst.c
+++ bst/src/bst.c
@@ -3,42 +3,43 @@
 #include "../inc/bst.h"
 
 struct binarytree{
-
 	int item;
 	BinaryTree* left;
-	BinaryTree* right;	
-
+	BinaryTree* right;
 };
 
-BinaryTree* create_bst_node(int item){
+BinaryTree* create_empty_bst() {
+  return NULL;
+}
 
-	BinaryTree* bt = (BinaryTree*)malloc(sizeof(BinaryTree));
+BinaryTree* create_bst_node(int item){
+	BinaryTree* bt = (BinaryTree*) malloc(sizeof(BinaryTree));
 	bt->item = item;
 	bt->left = NULL;
 	bt->right = NULL;
-	return bt;
 
+	return bt;
 }
 
-BinaryTree* add_branches(BinaryTree* bt, int item){
+BinaryTree* add_branch(BinaryTree* bt, int item){
+	if(bt == NULL) return create_bst_node(item);
 
-	
 	if(bt->item == item) return bt;
 
 	else if(item < bt->item){
 		if(bt->left == NULL)
 			bt->left = create_bst_node(item);
-			
-		else 
-			bt->left = add_branches(bt->left, item);
+
+		else
+			bt->left = add_branch(bt->left, item);
 
 	}
 	else if(item >= bt->item){
 		if(bt->right == NULL)
 			bt->right = create_bst_node(item);
-			
+
 		else
-			bt->right = add_branches(bt->right, item);
+			bt->right = add_branch(bt->right, item);
 
 	}
 
@@ -48,10 +49,10 @@ BinaryTree* add_branches(BinaryTree* bt, int item){
 
 void free_bst(BinaryTree* bt){
   	if(bt == NULL) return;
- 
+
   	free_bst(bt->left);
   	free_bst(bt->right);
- 
+
   	free(bt);
  	return;
 }
@@ -59,42 +60,41 @@ void free_bst(BinaryTree* bt){
 void print_bst(BinaryTree* bt){
 
 	if(bt == NULL) return;
- 	
+
 	printf("%d ", bt->item);
 
  	print_bst(bt->left);
  	print_bst(bt->right);
- 
+
  	return;
 
 }
 
-int search_bst(BinaryTree* bt, int item){
-  	int i = 0;
-  	if(bt == NULL) return 0;
-	if(bt->item == item){
-		++i;
-		return i;
-	} 
-  	else if(item < bt->item) search_bst(bt->left, item);
-  	else if(item > bt->item) search_bst(bt->right, item);
- 
+BinaryTree* search_bst(BinaryTree* bt, int item){
+	if(bt == NULL) return NULL;
+
+  if(bt->item == item) return bt;
+  else if(item < bt->item) search_bst(bt->left, item);
+  else if(item > bt->item) search_bst(bt->right, item);
 }
 
 BinaryTree* remove_bst_node(BinaryTree* bt, int item){
-
 	BinaryTree* first = search_bst(bt, item);
 	BinaryTree* second;
+
 	if(first != NULL){
 		BinaryTree* last = first->left;
-		if(last->right != NULL)
-			do{
-				second = last;
-				last = last->right;	
-			}while(last->right != NULL);
-			
+
+		if(last->right != NULL) {
+      do {
+        second = last;
+        last = last->right;
+      } while(last->right != NULL);
+    }
+
 		first->item = last->item;
 		second->right = NULL;
-		free(last);
+
+    free(last);
 	}
 }
